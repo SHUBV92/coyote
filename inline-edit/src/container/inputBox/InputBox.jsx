ba837c1385b0037193e390ce98ebd1b84a76aa4a
your Shubinder Virk <shubinder92@gmail.com>
2020-10-02 18:18:50 +0100

    5th Commit: Debugging

--- inline-edit/src/container/inputBox/InputBox.jsx
+++ inline-edit/src/container/inputBox/InputBox.jsx
@@ -7,6 +7,38 @@ const InputBox = () => {
     showInputField,
     setShowInputField,
   ] = useState(true);
+  const [apiResult, setApiResult] = useState("");
+
+  const validatedApi = (value) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (keyword.length < 10) {
+          resolve(value);
+        }
+        if (keyword.length > 10) {
+          reject("Error");
+        }
+      }, 3000);
+    });
+  };
+
+  const onSubmit = async (event) => {
+   event.preventDefault()
+    // setLoading(true);
+
+    try {
+      const apiResponse = await validatedApi();
+      if (apiResponse === "Success") {
+        //   setLoading(false);
+        setApiResult(true);
+        setShowInputField(false);
+      }
+    } catch (err) {
+    //   setLoading(false);
+      setApiResult(false);
+      setShowInputField(false);
+    }
+  };
 
   return (
     <Container
